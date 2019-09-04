16a09954f9a9f1fe52cd674c175a002fac4a3633
Jin Hyung Park <41055141+jypthemiracle@users.noreply.github.com>
2019-09-04 23:36:27 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <h1>Todo</h1>
       <TodoList />
     </div>
   );
