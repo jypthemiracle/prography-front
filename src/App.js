812347243a86ddadab56c28062e24b3c73a13668
Jin Hyung Park <41055141+jypthemiracle@users.noreply.github.com>
2019-09-04 23:31:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,97 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import axios from "axios";
-import uuid from "uuid";
+import TodoList from './components/TodoList';
 
-function Todo({todo, id, completeTodo, removeTodo}) {
+function App() {
   return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.status ? "line-through" : ""}}
-    >
-      {todo.text}
-      <div>
-        <button onClick={() => completeTodo(id)}>Complete</button>
-        <button onClick={() => removeTodo(id)}>x</button>
-      </div>
-    </div>
-  )
-}
-
-function TodoForm({addTodo}){
-  const [value, setValue] = useState("");
-  const handleSubmit = e => {
-    e.preventDefault();
-    if(!value) return;
-    addTodo(value);
-    setValue("");
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
-
-function App(){
-  const [state, setState] = useState({
-    todos:[]
-  });
-
-  //fetch API data
-  const getData = () => {
-    axios
-    .get("https://killsanghyuck.github.io/prography_5th_front/todoDummy.json")
-    .then(res => setState({todos:res.data.body}));
-  };
-  useEffect(() => {
-    getData();
-  }, []);
-
-  //Adding todo list
-  const addTodo = title => {
-    const newTodos = [...state, {
-      title,
-      id: uuid.v4(),
-      status: "todo"
-    }];
-    setState(newTodos);
-  };
-
-  //removing todo list
-  const removeTodo = id => {
-    const newTodos = [...state];
-    newTodos.splice(id, 1);
-    setState(newTodos);
-  };
-
-  //completing todo list
-  const completeTodo = id => {
-    const newTodos = [...state];
-    newTodos[id].status = "complete";
-    setState(newTodos);
-  };
-
-  return (
-    <div className="app">
-      <div className="todo-list">
-        {state.map((todo, index) => (
-          <Todo
-            key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
-          />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoList />
     </div>
   );
 }
+
 export default App;
